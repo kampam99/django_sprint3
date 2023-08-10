6803c40ae71b9d0543c9004c01ed8fbd7f77e53c
Мартиросян Арман <martirosyan.a2023@yandex.ru>
2023-08-10 19:19:14 +0700

    first

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,29 +1,78 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
-
-class BaseModel(models.Model):
-    created_at = models.DateTimeField('Добавлено', auto_now_add=True)
+class IsPublishedAndCreatedAt(models.Model):
     is_published = models.BooleanField(
-        'Опубликовано',
+        verbose_name='Опубликовано',
         default=True,
         help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+    created_at = models.DateTimeField(
+        verbose_name='Добавлено',
+        auto_now_add=True
     )
 
     class Meta:
         abstract = True
 
 
-class Category(BaseModel):
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
-    description = models.TextField(verbose_name='Описание')
-    slug = models.SlugField(unique=True, verbose_name='Идентификатор',
-                            help_text='Идентификатор страницы для URL; '
-                            'разрешены символы латиницы, цифры, дефис и '
-                            'подчёркивание.')
+class Post(IsPublishedAndCreatedAt):
+    title = models.CharField(
+        verbose_name='Заголовок',
+        max_length=256
+    )
+    text = models.TextField(
+        verbose_name='Текст'
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        help_text=('Если установить дату и время в '
+                   'будущем — можно делать отложенные публикации.')
+    )
+    author = models.ForeignKey(
+        verbose_name='Автор публикации',
+        to=User,
+        on_delete=models.CASCADE
+    )
+    location = models.ForeignKey(
+        verbose_name='Местоположение',
+        to='Location',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL
+    )
+    category = models.ForeignKey(
+        verbose_name='Категория',
+        to='Category',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts'
+    )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.title
+
+
+class Category(IsPublishedAndCreatedAt):
+    title = models.CharField(
+        verbose_name='Заголовок',
+        max_length=256
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор',
+        unique=True,
+        help_text=('Идентификатор страницы для URL; '
+                   'разрешены символы латиницы, цифры, дефис и подчёркивание.')
+    )
 
     class Meta:
         verbose_name = 'категория'
@@ -33,8 +82,11 @@ class Category(BaseModel):
         return self.title
 
 
-class Location(BaseModel):
-    name = models.CharField(max_length=256, verbose_name='Название места')
+class Location(IsPublishedAndCreatedAt):
+    name = models.CharField(
+        verbose_name='Название места',
+        max_length=256
+    )
 
     class Meta:
         verbose_name = 'местоположение'
@@ -42,36 +94,3 @@ class Location(BaseModel):
 
     def __str__(self):
         return self.name
-
-
-class Post(BaseModel):
-    title = models.CharField(max_length=256, verbose_name='Заголовлок')
-    text = models.TextField(verbose_name='Текст')
-    pub_date = models.DateTimeField(verbose_name='Дата и время публикации',
-                                    help_text='Если установить дату '
-                                    'и время в будущем — можно делать '
-                                    'отложенные публикации.')
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name='Автор публикации'
-    )
-    location = models.ForeignKey(
-        Location,
-        on_delete=models.SET_NULL,
-        null=True,
-        verbose_name='Местоположение'
-    )
-    category = models.ForeignKey(
-        Category,
-        on_delete=models.SET_NULL,
-        null=True,
-        verbose_name='Категория'
-    )
-
-    class Meta:
-        verbose_name = 'публикация'
-        verbose_name_plural = 'Публикации'
-
-    def __str__(self):
-        return self.title
